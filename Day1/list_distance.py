f543c6179d219b7b37055cb152739cd6b6cdee1d
htscode <hannah.terborg@web.de>
2024-12-01 23:21:48 +0200

    Advent of Code Day 1 solution

--- Day1/list_distance.py
+++ Day1/list_distance.py
@@ -1,1 +1,34 @@
-#test
+import numpy as np
+import csv
+
+
+
+a=[]
+b=[]
+with open('input.csv',newline='') as file:
+    data_reader = csv.reader(file, delimiter=' ')
+    for row in data_reader:
+        a.append(int(row[0]))
+        b.append(int(row[-1]))
+# test case
+#a = [1,2,2,2,3,26,6,1]
+#b=[2,3,3,2,1,4,2,1]
+
+
+# sort both lists
+sorted_a = np.sort(a)
+sorted_b = np.sort(b)
+# built difference
+list_distance = np.sum(np.abs(sorted_a-sorted_b))
+print(list_distance)
+# similarity score
+b_numbers, counts = np.unique(sorted_b,return_counts=True)
+look_up  =dict(zip(b_numbers, counts))
+i = 0
+similarity_index = 0
+for number in sorted_a:
+    if number in look_up:
+        multiplier = look_up[number]
+        similarity_index += number * multiplier
+print(similarity_index)
+print('Yeah')
